4482165382d3d3123437de532f2ba7b46b1366c3
Patrik Slovic <patrik.slovic@3ofthem.eu>
2025-07-24 17:18:11 +0200

    Change sizes attribute of horizontal carousel

--- components/Project/ProjectCarouselHorizontal.vue
+++ components/Project/ProjectCarouselHorizontal.vue
@@ -27,7 +27,7 @@ const currentImageTitle = computed(() => props.images.at(currentSlide.value)?.ti
         <div class="relative aspect-[16/9] w-full">
           <NuxtImg
             :src="image.src"
-            sizes="calc(100vw - 40px) lg:976px"
+            sizes="90vw lg:976px"
             quality="85"
             :alt="image.title"
             class="w-full h-full object-cover pointer-events-none select-none"
