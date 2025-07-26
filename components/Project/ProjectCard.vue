8c5a11f1ef6db4d830e6250f365e99398267aee8
Patrik Slovic <patrik.slovic@3ofthem.eu>
2025-07-26 02:53:54 +0200

    Try to fix CLS

--- components/Project/ProjectCard.vue
+++ components/Project/ProjectCard.vue
@@ -12,19 +12,22 @@ const imgAlt = computed(() => `${props.project.name} preview`)
 <template>
   <NuxtLink
     :to="{ name: 'projects-slug', params: { slug: project.slug } }"
-    class="flex flex-col sm:flex-row overflow-hidden rounded cursor-pointer group glass-effect"
+    class="flex flex-col sm:flex-row overflow-hidden rounded cursor-pointer group sm:min-h-[220px] md:min-h-[266.66px] glass-effect"
   >
     <NuxtImg
       :src="project.previewImg"
       :alt="imgAlt"
       sizes="90vw sm:330px md:400px"
       preset="project"
+      width="400"
+      height="266.66"
       :fetchpriority="index < 2 ? 'high' : 'auto'"
       class="object-cover aspect-3/2 w-full h-auto transition-all duration-300 sm:w-[330px] md:w-[400px] lg:grayscale-100 lg:group-hover:grayscale-0"
     />
+
     <!-- TODO: Add min-height when image is not still loaded -->
     <!-- TODO: Also find out why images are shown as 2x (densities?) -->
-    <div class="p-4 md:p-8 sm:min-h-[220px] md:min-h-[266.66px]">
+    <div class="p-4 md:p-8">
       <div class="flex gap-4 justify-between">
         <MyHeading :level="4" as="h2" class="font-medium max-w-[75%] md:max-w-[85%]">{{
           project.name
