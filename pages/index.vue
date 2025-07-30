b7c20932654e7ae24bd36fe4e8647128ae4ddf7d
Patrik Slovic <patrik.slovic@3ofthem.eu>
2025-07-30 23:28:14 +0200

    Add span for homepage paragraph

--- pages/index.vue
+++ pages/index.vue
@@ -17,8 +17,8 @@ useHead({
     <MyHeading :level="1" class="font-display font-bold"
       >Shaping the Web with <span class="text-yellow-600">Passion</span></MyHeading
     >
-    <p class="mt-8 lg:mt-16 text-xl text-grey-200">
-      I'm a web developer dedicated to crafting modern, interactive web applications, with a strong
+    <p class="mt-8 lg:mt-16 text-xl text-grey-200 leading-8">
+      I'm a <span class="border-b-3 border-yellow-600">frontend developer</span> dedicated to crafting modern, interactive web applications, with a strong
       emphasis on clean, maintainable code.
     </p>
 
@@ -30,7 +30,7 @@ useHead({
     <section
       class="mt-8 lg:mt-16 text-center space-y-6 bg-gradient-to-r from-yellow-600/10 to-orange-600/10 backdrop-blur-[1px] p-8 rounded-lg"
     >
-      <MyHeading :level="2" as="h2" class="font-bold">Ready to Work Together?</MyHeading>
+      <MyHeading :level="2" as="h3" class="font-bold">Ready to Work Together?</MyHeading>
       <p class="text-grey-200 max-w-xl mx-auto">
         I'm always interested in new opportunities and exciting projects. Let's discuss how we can
         bring your ideas to life.
